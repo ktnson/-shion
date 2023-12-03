8f9d18ff671b26f12ec7d4540fe8364e2abb9aea
ktnson <ichigo424shion@gmail.com>
2023-12-04 01:33:54 +0900

    Add 記事投稿作成画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,9 +12,19 @@ class PostController extends Controller
        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
 
     }
-    
+    /**
+     * *特定IDのpostを表示する
+     * 
+     * @params Object Post // 引数の$postはid=1のPostインスタンス
+     * @return Reposnse post view
+     */
     public function show(Post $post)
     {
         return view('posts/show')->with(['post' => $post]);
+        //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+    public function create()
+    {
+        return view('posts/create');
     }
 }

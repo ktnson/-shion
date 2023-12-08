1df5bf03ba1b2eb74b88a5bc9cae5c1c96a5bf03
ktnson <ichigo424shion@gmail.com>
2023-12-08 10:57:38 +0900

    Add ブログ投稿の編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,8 +13,26 @@ class PostController extends Controller
 
     }
     
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+    }
+    
     public function create()
     {
         return view('posts/create');
     }
+    
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request('post');
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
 }
--- resources/views/posts/edit.blade.1.php
+++ resources/views/posts/edit.blade.1.php
@@ -10,20 +10,21 @@
     </head>
     <body class='antialiased'>
         <h1>Blog Name</h1>
-       <form action='/posts' method='POST'>
+       <form action="/posts/{{ $post->id }}" method='POST'>
            @csrf
-           <div class='title'>
+           @method('PUT')
+           <div class='content__bodytitle'>
                <h2>Title</h2>
-               <input type='text' name=post[title] placeholder="タイトル"> 　
+               <input type='text' name=post[title] placeholder="タイトル" value="{{ $post->title }}"> 　
            </div>
-           <div class='body'>
+           <div class='content__body'>
                <h2>Body</h2>
-               <textarea name='post[body]' placeholder="今日も一日お疲れさまでした"></textarea>
+               <textarea name='post[body]' placeholder="今日も一日お疲れさまでした" {{ $post->body }}></textarea>
            </div>
-           <input type='submit' value='store'>
+           <input type='submit' value='update'>
        </form>
         <div class='footer'>
-            <a href="/">戻る</a>
+            <a href="/posts/{{ $post->id }}">戻る</a>
         </div>
     </body>
 </html>
